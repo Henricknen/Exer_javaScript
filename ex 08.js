8b7529752f09024da70f2dcd7d59094cbe4874aa
Luis Henrique Silva Ferreira <56833636+Henricknen@users.noreply.github.com>
2020-11-12 21:57:55 -0300

    extensão JS

---
+++ ex 08.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`Seu carro esta ${vel} Km por hora...`)
+if (vel > 60) {  6//Condição simples....
+    console.log('Voce ultrapassou a velocidade, MULTADO')
+}
+console.log(`Use o cinto de seguraça...`)
